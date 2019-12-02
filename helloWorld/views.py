33ddf8107cc3cf218ea5a0a5933bd24fc7afaeec
Adam <adamkberard@gmail.com>
2019-12-02 13:19:51 -0800

    it should now show the token if it logs in correctly

--- helloWorld/views.py
+++ helloWorld/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
-
+import requests
 
 def homePageView(request):
-    return HttpResponse('Hello, World!')
+
+    url = "https://capstone.api.roopairs.com/v0/auth/login/"
+    data = {
+              "username": "lordfarqaad@duloc.com",
+              "password": "pass4farquaad"
+           }
+    response = requests.post(url, json=data)
+    return HttpResponse(response.text)
