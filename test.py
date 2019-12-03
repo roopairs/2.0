8a62a2d2cff526d5d7d7ddbf82eee4eecaf98cf2
Adam <adamkberard@gmail.com>
2019-12-02 20:43:03 -0800

    $1

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 # File Name : test.py
 # Created By : Adam Berard
 # Creation Date : 02-12-2019
-# Last Modified : Mon Dec  2 20:13:44 2019
+# Last Modified : Mon Dec  2 20:36:03 2019
 # Description:
 
 ################################################################################
@@ -16,7 +16,7 @@ url = "http://localhost:8000/verticalAPI/"
 
 ################################################################################
 if __name__ == '__main__':
-   data = {"username": "Lord Farquaad",
-           "password": "hello world"}
+   data = {"username": "1tommy@gmail.com",
+           "password": "password"}
    x = requests.post(url, json=data)
    print(x.text)
--- verticalAPI/views.py
+++ verticalAPI/views.py
@@ -12,73 +12,54 @@ def testVert(request):
    user = request.data.get('username')
    password = request.data.get('password')
 
-   #email = "tommy@gmail.com"
-   #password = "password"
-   returnable = getDBInfo(user, password)
+   deleteTables()
+
+   returnable = loginAndGet(user, password)
    return Response(data=returnable)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def getDBInfo(email, password):
+def deleteTables():
     # Connect to an existing database
     conn = psycopg2.connect("postgres://ghxbuhvcqaekmf:e0ca350eb49c619ecece518cd258363ed6d801496d50c4ae7fffa6eecf639997@ec2-107-22-253-158.compute-1.amazonaws.com:5432/d1es3so922rir0", sslmode='require')
-
-
     # Open a cursor to perform database operations
     cur = conn.cursor()
 
     cur.execute("DELETE FROM prop_manager where true")
     cur.execute("DELETE FROM property where true")
+
+    # Make the changes to the database persistent
+    conn.commit()
+
+    # Close communication with the database
+    cur.close()
+    conn.close()
+
+def loginAndGet(email, password):
+    # Connect to an existing database
+    conn = psycopg2.connect("postgres://ghxbuhvcqaekmf:e0ca350eb49c619ecece518cd258363ed6d801496d50c4ae7fffa6eecf639997@ec2-107-22-253-158.compute-1.amazonaws.com:5432/d1es3so922rir0", sslmode='require')
+
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
     cur.execute("INSERT INTO prop_manager (LastName, FirstName, email, phone, password) VALUES ('Bergmann', 'Thomas', 'tommy@gmail.com', '8575552323', 'password');")
 
     select_sql = "SELECT managerId FROM prop_manager where email = \'" + email + "\' and password = \'" + password + "\';"
     cur.execute(select_sql)
-    manId = cur.fetchone()[0]
+
+    manId = cur.fetchone()
+
+    print(manId)
+    if(manId == None):
+       return "ERROR: There is no account with the username and password specified."
+    else:
+       manId = manId[0]
+
 
     insert_sql = "INSERT INTO property (address, SLID, numBath, numBed, maxTenats, manId) VALUES ('200 North Santa Rosa Street', 1, 3, 3, 6," + str(manId) + ");"
     cur.execute(insert_sql)
 
     insert_sql = "INSERT INTO property (address, SLID, numBath, numBed, maxTenats, manId) VALUES ('110 Orange Drive', 2, 2, 1.5, 3, " + str(manId) + ");"
     cur.execute(insert_sql)
-
-
 
     select_sql = "SELECT * FROM prop_manager where email = \'" + email + "\' and password = \'" + password + "\';"
     cur.execute(select_sql)
