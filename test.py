b7529b987117b8030c0fd5d6ac9ce8746327037e
Adam <adamkberard@gmail.com>
2019-12-04 19:47:46 -0800

    $1

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 # File Name : test.py
 # Created By : Adam Berard
 # Creation Date : 02-12-2019
-# Last Modified : Wed Dec  4 13:21:21 2019
+# Last Modified : Wed Dec  4 19:41:15 2019
 # Description:
 
 ################################################################################
@@ -17,8 +17,8 @@ url = "https://vertical-proto-homepairs.herokuapp.com/verticalAPI/"
 
 ################################################################################
 if __name__ == '__main__':
-   data = {"username": "tommy@gmail.com", "password": "pass4tommy"}
-   data = {"username": "dam@gmail.com", "password": "pass4adam"}
    data = {"username": "adam@gmail.com", "password": "pass4adam"}
+   data = {"username": "dam@gmail.com", "password": "pass4adam"}
+   data = {"username": "tommy@gmail.com", "password": "pass4tommy"}
    x = requests.post(url, json=data)
    print(x.text)
