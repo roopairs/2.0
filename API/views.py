7bb80c005768fd767b37641e82af560d5705a72e
Adam <adamkberard@gmail.com>
2020-01-16 11:18:18 -0800

    $1

--- API/views.py
+++ API/views.py
@@ -125,7 +125,8 @@ def tenantRegister(request):
                          pm = tenPM)
             ten.save()
             return Response(data=tenantLogin(request))
-      return Response(data=getTenant(tenantEmail, tenantPass))
+         return Response(data=returnError('too many props?')
+      return Response(data=getTenant(tenEmail, tenPass))
    else:
       return Response(data=returnError('incorrect11fields'))
 
