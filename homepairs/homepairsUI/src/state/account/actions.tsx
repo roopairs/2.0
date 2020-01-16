beca8c522b80c1dd9690aabbea07343aefe509df
Cesar Chacon <cesarfchacon@msn.com>
2020-01-16 12:40:38 -0800

    fixed UI

--- homepairs/homepairsUI/src/state/account/actions.tsx
+++ homepairs/homepairsUI/src/state/account/actions.tsx
@@ -112,12 +112,13 @@ export const loginForPM = (Email: String, Password: String, modalSetOffCallBack?
 
 export const generateAccountForTenant = (accountDetails: Account, password: String, modalSetOffCallBack?: (error?:String) => void, navigateMainCallBack?: () => void) => {
   return async (dispatch: (arg0: any) => void) => {
-      return await axios.post('', {
+      return await axios.post('http://homepairs-alpha.herokuapp.com/API/register/tenant/', {
         firstName: accountDetails.firstName, 
         lastName: accountDetails.lastName,
-        address: accountDetails.address, 
+        streetAddress: accountDetails.address, 
         city: accountDetails.city,
         email: accountDetails.email, 
+        phone: accountDetails.phone, 
         password: password, 
       })
       .then((response) => {
@@ -126,6 +127,7 @@ export const generateAccountForTenant = (accountDetails: Account, password: Stri
           dispatch(fetchProperties(response[responseKeys.DATA][responseKeys.PROPERTIES]))
           navigateMainCallBack()
         } else {
+          console.log(response);
           modalSetOffCallBack("Home Pairs was unable to log in. Please try again.")
         }
       })
@@ -138,11 +140,13 @@ export const generateAccountForTenant = (accountDetails: Account, password: Stri
 
 export const generateAccountForPM = (accountDetails: Account, password: String, modalSetOffCallBack?: (error?:String) => void, navigateMainCallBack?: () => void) => {
     return async (dispatch: (arg0: any) => void) => {
-      return await axios.post('', {
+      return await axios.post('http://homepairs-alpha.herokuapp.com/API/register/pm/', {
           firstName: accountDetails.firstName, 
           lastName: accountDetails.lastName,
           email: accountDetails.email, 
           phone: accountDetails.phone,
+          companyName: accountDetails.companyName, 
+          companyType: accountDetails.companyType,
           password: password, 
         })
         .then((response) => {
--- homepairs/homepairsUI/src/utility/index.tsx
+++ homepairs/homepairsUI/src/utility/index.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 7f44d63667571da9a1f9d8f1a5c43b60f839acec
 import * as EmailSyntaxVerification from './SyntaxVerification/EmailSyntaxVerification';
 
 export {EmailSyntaxVerification};
