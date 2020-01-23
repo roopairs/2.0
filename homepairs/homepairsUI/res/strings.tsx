3ffbdb57418007f0876576000163553445172ebc
Cesar Chacon <cesarfchacon@msn.com>
2020-01-23 14:19:52 -0800

    idk

--- homepairs/homepairsUI/res/strings.tsx
+++ homepairs/homepairsUI/res/strings.tsx
@@ -50,7 +50,6 @@ const strings = {
         newUserText: 'New to HomePairs? ',
         modal: 'Logging In...'
     },
-<<<<<<< HEAD
     signUpPage : {
       subtitle: 'Create your account',
       accountTypeRadioButton: {
@@ -72,31 +71,6 @@ const strings = {
       currentUserText: 'Already have an account? ',
       signUpHighlight: 'Sign In',
       modal: 'Signing Up...'
-=======
-    signUpPage: {
-        subtitle: 'Create your account',
-        accountTypeRadioButton: {
-            name: 'ACCOUNT TYPE',
-            tenant: 'Tenant',
-            landlord: 'Landlord'
-        },
-        inputForms: {
-            firstName: 'FIRST NAME',
-            lastName: 'LAST NAME',
-            email: 'EMAIL',
-            phone: 'PHONE',
-            password: 'PASSWORD',
-            confirmPassword: 'CONFIRM PASSWORD',
-            address: 'ADDRESS',
-            city: 'CITY',
-            companyName: 'COMPANY NAME',
-            companyType: 'COMPANY TYPE'
-        },
-        button: 'Sign Up',
-        currentUserText: 'Already have an account? ',
-        signUpHighlight: 'Sign In',
-        modal: 'Signing Up...'
->>>>>>> 60869f60e79bcabefe959d4c3345cc599ddb9669
     },
     logOut: 'Log Out'
 };
