86adf49a7dbfc4f39d0f84c5a1d7e9e9212629b1
Cesar Chacon <cesarfchacon@msn.com>
2020-01-23 13:42:06 -0800

    pulling for linter

--- homepairs/homepairsUI/src/Screens/Components/Modals/AddNewPropertyModal/AddNewPropertyModal.tsx
+++ homepairs/homepairsUI/src/Screens/Components/Modals/AddNewPropertyModal/AddNewPropertyModal.tsx
@@ -2,48 +2,31 @@ import React, { Component, ReactElement } from "react"
 import { Text, View, Modal, TouchableHighlight, StyleSheet } from "react-native"
 import Card, {CardProps} from '../../../../../src/Elements/Cards/Card';
 import HomePairColors from 'homepair-colors';
+import {ModalInjectedProps} from '../WithModal/WithModal';
 
-export type state = {modalVisible: boolean};
+type Props = ModalInjectedProps;
 
-type props = {isShown : boolean};
+type State = {};
 
 
-export class AddNewPropertyModal extends Component<props, state> {
+export class AddNewPropertyModal extends Component<Props, State> {
     constructor(props: any) {
         super(props);
-        this.state = {modalVisible: props.isShown};
     } 
     
-    setModalVisible(visible: boolean) {
-        this.setState({modalVisible: visible});
-    }
-
-    cardChildren : ReactElement[] = [
-        <Text>SUP YOU MADE A MODAL</Text>,
-        <TouchableHighlight onPress = {() => {
-            this.setModalVisible(false);
-        }}>
-            <Text>Close this ishhhh</Text>
-        </TouchableHighlight>
-    ];
-
-    cardProps : CardProps = {
-        children: this.cardChildren,
-        title: 'Add a New Property', 
-        titleStyle: styles.cardTitle,
-        showCloseButton: true, 
-        closeButtonPressedCallBack: () => {this.setModalVisible(false)}
-    };
-
     render() {
-        return <Modal animationType='slide' visible={this.state.modalVisible} transparent={false}>
-            <View>
-                {Card(this.cardProps)}
-            </View>
-        </Modal>
+        return <View>
+            <Card
+                showCloseButton= {true}
+                title= "Create New Property"
+                closeButtonPressedCallBack={() => this.props._onChangeModalVisibility(false)}
+                >
+            </Card>
+        </View>
     }
 }
 
 const styles = StyleSheet.create({
-    cardTitle: {}
+    container: {
+    }
 });
