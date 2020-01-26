6d4e14bd6098d20a35f22a79aabcd38799a7e198
Adam <adamkberard@gmail.com>
2020-01-26 15:52:19 -0800

    okay now hopefully it all works

--- homepairs/HomepairsApp/send/heroku.sh
+++ homepairs/HomepairsApp/send/heroku.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-cd ..
 
-git subtree push --prefix HomePairsApp heroku master
+git init
+git add .
+git commit -m 'deploy'
+heroku git:remote -a homepairs-alpha
+git push --force heroku master
+
