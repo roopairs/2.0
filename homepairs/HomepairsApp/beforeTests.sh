d8c664a2395ea3c10dde643d482e8008f4a5d81f
Adam <adamkberard@gmail.com>
2020-01-29 11:34:32 -0800

    annoother one

--- homepairs/HomepairsApp/beforeTests.sh
+++ homepairs/HomepairsApp/beforeTests.sh
@@ -6,7 +6,13 @@ psql -c "ALTER USER dummy WITH CREATEDB;"
 psql -c "CREATE DATABASE testboop OWNER dummy;"
 
 echo "cool"
+
+psql -c "\list"
+
+echo "cool 1"
+
 python manage.py migrate
+
 echo "beans"
 
 python manage.py runserver
