67053f73ad3533927997f38afb0cf273357d6442
adamkberard <adamkberard@gmail.com>
2020-01-29 12:55:02 -0800

    Delete admin.py

--- homepairs/homepairsApp/API/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import PropertyManager, Property, Tenant
-
-# Register your models here.
-
-admin.site.register(PropertyManager)
-admin.site.register(Property)
-admin.site.register(Tenant)
