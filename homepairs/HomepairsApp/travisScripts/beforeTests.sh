0dab723ee3955b0c5f462ef23a6735fb216dc9e4
Adam <adamkberard@gmail.com>
2020-01-29 12:07:54 -0800

    just reorganizing

--- homepairs/HomepairsApp/travisScripts/beforeTests.sh
+++ homepairs/HomepairsApp/travisScripts/beforeTests.sh
@@ -5,16 +5,6 @@ psql -c "ALTER USER dummy WITH CREATEDB;"
 
 psql -c "CREATE DATABASE testboop OWNER dummy;"
 
-echo "cool"
-
-psql -c "\list"
-
-echo "cool 1"
-
 python manage.py migrate
 
-echo "beans"
-
 python manage.py runserver &
-
-echo "beans are cool"
