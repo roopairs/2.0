e569d08c50a34ca54e93e451d5da6f4ca0f1a137
Adam <adamkberard@gmail.com>
2020-01-29 11:23:50 -0800

    now it will turn the server on

--- homepairs/HomepairsApp/beforeTests.sh
+++ homepairs/HomepairsApp/beforeTests.sh
@@ -4,3 +4,5 @@ psql -c "CREATE USER dummy WITH PASSWORD 'pass4dummy';"
 psql -c "ALTER USER dummy WITH CREATEDB;"
 
 psql -c "CREATE DATABASE test_dummybase OWNER dummy;"
+
+python manage.py runserver
