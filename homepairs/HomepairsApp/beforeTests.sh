c045144e7f8c6ad0502b3f4792a046bb76c43160
Sarah MacDougall <smacdoug@calpoly.edu>
2020-01-28 22:15:30 -0800

    change OWNER to username instead of password

--- homepairs/HomepairsApp/beforeTests.sh
+++ homepairs/HomepairsApp/beforeTests.sh
@@ -3,4 +3,4 @@ psql -c "CREATE USER dummy WITH PASSWORD 'pass4dummy';"
 
 psql -c "ALTER USER dummy WITH CREATEDB;"
 
-psql -c "CREATE DATABASE test_dummybase OWNER 'pass4dummy';"
+psql -c "CREATE DATABASE test_dummybase OWNER dummy;"
