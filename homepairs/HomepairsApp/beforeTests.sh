9ef6925c6f3558264af18ea4556a9b96667c541b
Sarah MacDougall <smacdoug@calpoly.edu>
2020-01-28 22:01:58 -0800

    separate db setup from install script

---
+++ homepairs/HomepairsApp/beforeTests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+psql -c "CREATE USER dummy WITH PASSWORD 'pass4dummy';"
+
+psql -c "ALTER USER dummy WITH CREATEDB;"
+
+psql -c "CREATE DATABASE test_dummybase OWNER 'pass4dummy':"
--- homepairs/HomepairsApp/installScript.sh
+++ homepairs/HomepairsApp/installScript.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 pip install -r requirements.txt
-
-psql -c "CREATE USER dummy WITH PASSWORD 'pass4dummy';"
-
-psql -c "ALTER USER dummy WITH CREATEDB;"
-
-psql -c "CREATE DATABASE test_dummybase OWNER 'pass4dummy':"
