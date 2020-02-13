8b56be0632a66d90bf4052ec26ef7d71c781bae8
Cesar Chacon <cesarfchacon@msn.com>
2020-02-12 23:29:48 -0800

    Added new test for validateForms function

--- homepairs/homepairsUI/tests/pages/Components/AddNewPropertyModal.test.tsx
+++ homepairs/homepairsUI/tests/pages/Components/AddNewPropertyModal.test.tsx
@@ -42,14 +42,17 @@ describe("Add New Property Modal", () => {
     const modalFunction = jest.fn(onChangeModalVisibility);
     const modalFunction2 = jest.fn(onChangeModalVisibility);
     const modalFunction3 = jest.fn(onChangeModalVisibility);
+    const modalFunction4 = jest.fn(onChangeModalVisibility);
     const createFunction = jest.fn(onCreateProperty);
     const createFunction2 = jest.fn(onCreateProperty);
     const createFunction3 = jest.fn(onCreateProperty);
+    const createFunction4 = jest.fn(onCreateProperty);
 
     const wrapper = shallow(<AddNewPropertyModalBase {...props} onChangeModalVisibility={modalFunction} onCreateProperty={createFunction}/>);
     const rendered = renderer.create(<AddNewPropertyModalBase {...props} onChangeModalVisibility={modalFunction} onCreateProperty={createFunction}/>);
     const rendered1 = render(<AddNewPropertyModalBase {...props} onChangeModalVisibility={modalFunction2} onCreateProperty={createFunction2}/>);
     const rendered2 = render(<AddNewPropertyModalBase {...props} onChangeModalVisibility={modalFunction3} onCreateProperty={createFunction3}/>);
+    const rendered3 = render(<AddNewPropertyModalBase {...props} onChangeModalVisibility={modalFunction4} onCreateProperty={createFunction4}/>);
 
     it("Test for proper components", () => {
         expect(wrapper.find(InputForm)).toHaveLength(6);
@@ -61,6 +64,37 @@ describe("Add New Property Modal", () => {
         rendered.root.props.onCreateProperty();
         expect(modalFunction.mock.calls).toHaveLength(1);
         expect(createFunction.mock.calls).toHaveLength(1);
+    });
+
+    it ("Test validate forms", () => {
+        const {getAllByTestId, getByType} = rendered3;
+        const inputs = getAllByTestId('userTextInput');
+        const modal = getByType(AddNewPropertyModalBase);
+        const address = inputs[0];
+        const city = inputs[1];
+        const state = inputs[2];
+        const tenants = inputs[3];
+        const bedrooms = inputs[4];
+        const bathrooms = inputs[5];
+
+        fireEvent.changeText(address, '');
+        fireEvent.changeText(city, '');
+        fireEvent.changeText(state, '');
+        fireEvent.changeText(tenants, 'asdf');
+        fireEvent.changeText(bedrooms, 'asdf');
+        fireEvent.changeText(bathrooms, 'asdf');
+
+        
+        expect(modal.instance.validateForms()).toBeFalsy();
+
+        fireEvent.changeText(address, '123 Testing St.');
+        fireEvent.changeText(city, 'San Luis Obispo');
+        fireEvent.changeText(state, 'CA');
+        fireEvent.changeText(tenants, '5');
+        fireEvent.changeText(bedrooms, '4');
+        fireEvent.changeText(bathrooms, '2');
+
+        expect(modal.instance.validateForms()).toBeTruthy();
     });
 
     it("Test behavior when closed", () => {
