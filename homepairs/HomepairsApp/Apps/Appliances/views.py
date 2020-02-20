87c01cf7614c5dbf377c1a654820f03d3e3012e8
tbbergmann <tommy.bergmann2332@gmail.com>
2020-02-19 22:28:02 -0800

    fixing app code

--- homepairs/HomepairsApp/Apps/Appliances/views.py
+++ homepairs/HomepairsApp/Apps/Appliances/views.py
@@ -30,6 +30,7 @@ PM_SQUISH = 'This email is associated with more than one pm'
 INVALID_PROPERTY = 'Invalid property'
 PROPERTY_ALREADY_EXISTS = 'Property given already exists'
 NON_FIELD_ERRORS = 'non_field_errors'
+APPLIANCE_DOESNT_EXIST = 'Appliance does not exist.'
 PROPERTY_DOESNT_EXIST = 'Property does not exist.'
 NOT_PROP_OWNER = 'You are not the property owner'
 TOKEN = 'token'
@@ -41,8 +42,60 @@ BASE_URL = 'https://capstone.api.roopairs.com/v0/'
 ################################################################################
 # Helper Functions
 #
+def checkRequired(required, request):
+   missingFields = []
+   for term in required:
+       if(term not in request.data):
+           missingFields.append(term)
+   return missingFields
 
 
+def returnError(error):
+   return {STATUS: FAIL, ERROR: error}
+
+
+def missingError(missingFields):
+   finalErrorString = INCORRECT_FIELDS + ": "
+   for field in missingFields:
+       finalErrorString += field + " "
+   return returnError(finalErrorString.strip())
+
+@api_view(['GET', 'POST'])
+def createAppliance(request):
+   # roopairs api does not have a section for appliances yet
+   # url = BASE_URL + 'service-locations/'
+
+   required = ['name', 'description', 'location', 'address', 'city', 'state', 'token']
+
+   missingFields = checkRequired(required, request)
+
+   if(len(missingFields) == 0):
+       name = request.data.get('name')
+       description = request.data.get('description')
+       location = request.data.get('location')
+       streetAddress = request.data.get('address')
+       city = request.data.get('city')
+       state = request.data.get('state')
+
+       propList = Property.objects.filter(streetAddress=streetAddress, city=city, state=state)
+       if propList.exists():
+           prop = propList[0]
+           app = Appliance(name=name,
+                           description=description,
+                           location=location,
+                           place=prop)
+           app.save()
+           data = {
+                   STATUS: SUCCESS
+                  }
+           return Response(data=data)
+       else:
+           return Response(data=returnError(PROPERTY_DOESNT_EXIST))
+   else:
+       return Response(data=missingError(missingFields))
+
+
+##############################################################
 @api_view(['GET', 'POST'])
 def createAppliance(request):
     if(len(missingFields) == 0):
@@ -137,63 +190,3 @@ def deleteAppliance(request):
             return Response(data=returnError(APPLIANCE_DOESNT_EXIST))
     else:
         return Response(data=missingError(missingFields))
-
-
-
-
-
-
-
-
-
-#def checkRequired(required, request):
-#    missingFields = []
-#    for term in required:
-#        if(term not in request.data):
-#            missingFields.append(term)
-#    return missingFields
-#
-#
-#def returnError(error):
-#    return {STATUS: FAIL, ERROR: error}
-#
-#
-#def missingError(missingFields):
-#    finalErrorString = INCORRECT_FIELDS + ": "
-#    for field in missingFields:
-#        finalErrorString += field + " "
-#    return returnError(finalErrorString.strip())
-#
-#@api_view(['GET', 'POST'])
-#def createAppliance(request):
-#    # roopairs api does not have a section for appliances yet
-#    # url = BASE_URL + 'service-locations/'
-#
-#    required = ['name', 'description', 'location', 'address', 'city', 'state', 'token']
-#
-#    missingFields = checkRequired(required, request)
-#
-#    if(len(missingFields) == 0):
-#        name = request.data.get('name')
-#        description = request.data.get('description')
-#        location = request.data.get('location')
-#        streetAddress = request.data.get('address')
-#        city = request.data.get('city')
-#        state = request.data.get('state')
-#
-#        propList = Property.objects.filter(streetAddress=streetAddress, city=city, state=state)
-#        if propList.exists():
-#            prop = propList[0]
-#            app = Appliance(name=name,
-#                            description=description,
-#                            location=location,
-#                            place=prop)
-#            app.save()
-#            data = {
-#                    STATUS: SUCCESS
-#                   }
-#            return Response(data=data)
-#        else:
-#            return Response(data=returnError(PROPERTY_DOESNT_EXIST))
-#    else:
-#        return Response(data=missingError(missingFields))
