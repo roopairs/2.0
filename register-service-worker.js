2caf74e69632de33b8ca40aec4f39cf5d01d9b11
ItisEeron <eerongrant@gmial.com>
2020-04-13 22:51:34 -0700

    Updates

--- register-service-worker.js
+++ register-service-worker.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker
-      .register('/expo-service-worker.js', { scope: '/' })
+      .register('/2.0/expo-service-worker.js', { scope: '/2.0/' })
       .then(function(info) {
         // console.info('Registered service-worker', info);
       })
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.88cecf0f36e706255ee724c2be8aa0ae.js"
+  "/2.0/precache-manifest.16634c1982165abe0ebf949b4e5edf72.js"
 );
 
 workbox.skipWaiting();
@@ -29,7 +29,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("index.html", {
+workbox.routing.registerNavigationRoute("/2.0/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
