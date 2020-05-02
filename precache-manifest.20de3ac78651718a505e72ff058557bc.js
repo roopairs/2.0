a060eab379cbd32049129dc49802ce05edf0f8f3
ItisEeron <eerongrant@gmial.com>
2020-05-01 18:54:58 -0700

    Updates

---
+++ precache-manifest.20de3ac78651718a505e72ff058557bc.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e6f46af8f3d5a92fe9a",
+    "url": "/2.0/static/js/app.af405e39.chunk.js"
+  },
+  {
+    "revision": "394e48a3f414464a0568",
+    "url": "/2.0/static/js/runtime~app.1a737d44.js"
+  },
+  {
+    "revision": "42e6622f83a9582b6eb1",
+    "url": "/2.0/static/js/2.77025e50.chunk.js"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "/2.0/static/media/star.004c2bbb.png"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "/2.0/static/media/heart.2327736b.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "/2.0/static/media/rocket.51671417.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "/2.0/static/media/bell.a7b9dc9d.png"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/2.0/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/2.0/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/2.0/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/2.0/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/2.0/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/2.0/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/2.0/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/2.0/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/2.0/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "900caa7db580afb9489434ed9ce5c978",
+    "url": "/2.0/./fonts/Nunito-Black.ttf"
+  },
+  {
+    "revision": "ee649ba9c9d523c33b75d1f2bca5bf75",
+    "url": "/2.0/./fonts/Nunito-BlackItalic.ttf"
+  },
+  {
+    "revision": "a8c930b08cd5fda37ea9f1c105d1ac61",
+    "url": "/2.0/./fonts/Nunito-Bold.ttf"
+  },
+  {
+    "revision": "6c403b74f57bc9b80c05ba935864934d",
+    "url": "/2.0/./fonts/Nunito-BoldItalic.ttf"
+  },
+  {
+    "revision": "14f2f203c4644e945a9e80e4d8f9a615",
+    "url": "/2.0/./fonts/Nunito-ExtraBold.ttf"
+  },
+  {
+    "revision": "21becea3582b0515c5ad505d48b44a38",
+    "url": "/2.0/./fonts/Nunito-ExtraBoldItalic.ttf"
+  },
+  {
+    "revision": "a2f61593a1f33e5088345eccbb69c3f9",
+    "url": "/2.0/./fonts/Nunito-ExtraLight.ttf"
+  },
+  {
+    "revision": "d3e50bba9cc069ff5cc0bed2932d2184",
+    "url": "/2.0/./fonts/Nunito-ExtraLightItalic.ttf"
+  },
+  {
+    "revision": "5793817ee2744f2008c0e7a38e641569",
+    "url": "/2.0/./fonts/Nunito-Italic.ttf"
+  },
+  {
+    "revision": "f7fb34d1eab74a26abdb832e3db8dbde",
+    "url": "/2.0/./fonts/Nunito-Light.ttf"
+  },
+  {
+    "revision": "67ae90d1c81a5007876731437877e736",
+    "url": "/2.0/./fonts/Nunito-LightItalic.ttf"
+  },
+  {
+    "revision": "e9f64790b131c08d6b34a9ecdc453876",
+    "url": "/2.0/./fonts/Nunito-Regular.ttf"
+  },
+  {
+    "revision": "bb52b80a026d5b8ab09c4640b711153a",
+    "url": "/2.0/./fonts/Nunito-SemiBold.ttf"
+  },
+  {
+    "revision": "e020c7e6a9cc862e505fade06649899d",
+    "url": "/2.0/./fonts/Nunito-SemiBoldItalic.ttf"
+  },
+  {
+    "revision": "d948a69f4a74738c3fa03c036f027164",
+    "url": "/2.0/static/media/blender.d948a69f.png"
+  },
+  {
+    "revision": "f645d3edbb6b3271c0f6bd336e592811",
+    "url": "/2.0/static/media/bolt.f645d3ed.png"
+  },
+  {
+    "revision": "15f6ce85d9a8e10289dea5a2b12e453a",
+    "url": "/2.0/static/media/calendar.15f6ce85.png"
+  },
+  {
+    "revision": "7bb411a8e53a8f78e76fc6f81dd977f1",
+    "url": "/2.0/static/media/clock.7bb411a8.png"
+  },
+  {
+    "revision": "b0627cefa70e551e857d59ad3a69bf91",
+    "url": "/2.0/static/media/defaultProperty.b0627cef.png"
+  },
+  {
+    "revision": "e6b2927cad881364d5e877cd28f8f35f",
+    "url": "/2.0/static/media/downArrow.e6b2927c.png"
+  },
+  {
+    "revision": "65dbc9ae5f63f69068416c526a509849",
+    "url": "/2.0/static/media/fan.65dbc9ae.png"
+  },
+  {
+    "revision": "81c42269299cd7ce4080185a80c1a904",
+    "url": "/2.0/static/media/hamburger.81c42269.png"
+  },
+  {
+    "revision": "f82b34f900882c5120a1bfbf6df22a27",
+    "url": "/2.0/static/media/icon.f82b34f9.png"
+  },
+  {
+    "revision": "6fb8ba2114f87d2f9b42d55833995478",
+    "url": "/2.0/static/media/roopairsLogo.6fb8ba21.png"
+  },
+  {
+    "revision": "f4d2d34795f044f782e7573f68d13840",
+    "url": "/2.0/static/media/splash.f4d2d347.png"
+  },
+  {
+    "revision": "20c7011717c51d04e19f59abc5eafc8e",
+    "url": "/2.0/static/media/testLogo.20c70117.png"
+  },
+  {
+    "revision": "fbd4656dbdc235f641c9a49fdd5fde1b",
+    "url": "/2.0/static/media/tint.fbd4656d.png"
+  },
+  {
+    "revision": "ff197dc9c1f7b5ce69272d0591540561",
+    "url": "/2.0/static/media/trash.ff197dc9.png"
+  },
+  {
+    "revision": "b8d8110c1417bae8f7f5e82f7f6b74d0",
+    "url": "/2.0/static/media/upArrow.b8d8110c.png"
+  },
+  {
+    "revision": "2cfe80253b6035a7feb6d5c77b934a35",
+    "url": "/2.0/static/media/servprov.2cfe8025.png"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/2.0/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/2.0/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/2.0/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/2.0/expo-service-worker.js"
+  },
+  {
+    "revision": "e10ce0552ee7802193950fa9181bd961",
+    "url": "/2.0/favicon-16.png"
+  },
+  {
+    "revision": "b333dcaccf681a69a1b0a5058aa06fcb",
+    "url": "/2.0/favicon-32.png"
+  },
+  {
+    "revision": "62273473911e68575ace8d48f6ebd399",
+    "url": "/2.0/favicon.ico"
+  },
+  {
+    "revision": "913e826db5eaeb7c3b148eed716c8b65",
+    "url": "/2.0/index.html"
+  },
+  {
+    "revision": "fa42ca52384517e8b57affdd480f819b",
+    "url": "/2.0/manifest.json"
+  },
+  {
+    "revision": "ab901f55b3a9517e27dc7a838867d126",
+    "url": "/2.0/register-service-worker.js"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0532987e5470d462cfe222f0d6cf5dbc.js"
+  "/2.0/precache-manifest.20de3ac78651718a505e72ff058557bc.js"
 );
 
 workbox.skipWaiting();
@@ -29,7 +29,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("index.html", {
+workbox.routing.registerNavigationRoute("/2.0/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
