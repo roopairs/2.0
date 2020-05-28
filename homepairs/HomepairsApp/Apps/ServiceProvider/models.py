49aca3fb73e80ccb9a5c5a5dc67c2f0e641af0df
Thomas Bergmann <tommy.bergmann2332@gmail.com>
2020-05-28 16:12:24 -0500

    undoing model stuff because it breaks dungeon

--- homepairs/HomepairsApp/Apps/ServiceProvider/models.py
+++ homepairs/HomepairsApp/Apps/ServiceProvider/models.py
@@ -12,7 +12,7 @@ class ServiceProvider(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=254, null=True)
     phoneNum = models.CharField(max_length=128, null=True)
-    rooId = models.CharField(max_length=128, null=True)
+    #rooId = models.CharField(max_length=128, null=True)
     website = models.CharField(max_length=200, null=True)
     logo = models.CharField(max_length=200, null=True)
     address = models.CharField(max_length=100, null=True)
@@ -31,7 +31,7 @@ class ServiceProvider(models.Model):
                   "name": self.name,
                   "email": self.email,
                   "phoneNum": self.phoneNum,
-                  "rooId": self.rooId,
+                  #"rooId": self.rooId,
                   "contractLic": self.contractLic,
                   "skills": self.skills,
                   "website": self.website,
