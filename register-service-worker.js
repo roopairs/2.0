5a3555a9857659e4a28d9b3a9e1aa90b74d74243
ItisEeron <eerongrant@gmial.com>
2020-05-28 20:41:28 -0700

    Updates

--- register-service-worker.js
+++ register-service-worker.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker
-      .register('/2.0/expo-service-worker.js', { scope: '/2.0/' })
+      .register('/expo-service-worker.js', { scope: '/' })
       .then(function(info) {
         // console.info('Registered service-worker', info);
       })
